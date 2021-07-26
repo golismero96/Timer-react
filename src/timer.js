3e49c3ed1f74648496c4d264366cb72625e8db55
golismero <golismero96@gmail.com>
2021-07-26 23:31:06 +0430

    small update

--- src/timer.js
+++ src/timer.js
@@ -4,63 +4,68 @@ import React, {useState} from 'react';
 const Timer = () => {
     const [timer, setTimer] = useState("00:00:00");
     const [time, setTime] = useState(0);
+    const [stoptime, setStoptime] = useState(false);
+    const [running, setRunning] = useState(true);
     var hr = 0;
     var min = 0;
     var sec = 0;
-    var stoptime = true;
-    var dontStart = false;
+    // var stoptime = false;
+    // var running = true;
     
-    function timerCycle() {
-        if (!stoptime) {
-            sec = parseInt(sec);
-            min = parseInt(min);
-            hr = parseInt(hr);
+    const timerCycle = () => {
+        sec = parseInt(sec);
+        min = parseInt(min);
+        hr = parseInt(hr);
 
-            sec = sec + 1;
+        sec = sec + 1;
 
-            if (sec === 60) {
-                min = min + 1;
-                sec = 0;
-            }
-            if (min === 60) {
-                hr = hr + 1;
-                min = 0;
-                sec = 0;
-            }
-
-            if (sec < 10 || sec === 0) {
-                sec = '0' + sec;
-            }
-            if (min < 10 || min === 0) {
-                min = '0' + min;
-            }
-            if (hr < 10 || hr === 0) {
-                hr = '0' + hr;
-            }
-
-            setTimer(hr + ':' + min + ':' + sec);
-
-            setTime(setTimeout( timerCycle, 1000));
+        if (sec === 60) {
+            min = min + 1;
+            sec = 0;
         }
+        if (min === 60) {
+            hr = hr + 1;
+            min = 0;
+            sec = 0;
+        }
+
+        if (sec < 10 || sec === 0) {
+            sec = '0' + sec;
+        }
+        if (min < 10 || min === 0) {
+            min = '0' + min;
+        }
+        if (hr < 10 || hr === 0) {
+            hr = '0' + hr;
+        }
+
+        setTimer(hr + ':' + min + ':' + sec);
+
+        setTime(setTimeout( timerCycle, 1000));
     }
     const startTimer = () => {
-        if (stoptime && !dontStart) {
-            stoptime = false;
-            dontStart = true;
+        if (!stoptime && running) {
+            // stoptime = true;
+            // running = false;
+            setStoptime(true);
+            setRunning(false);
             timerCycle();
         }
     }
     const stopTimer = () => {
         if (stoptime) {
-            stoptime = true;
-            dontStart = false;
+            // stoptime = false;
+            // running = true;
+            setStoptime(false);
+            setRunning(true);
             clearTimeout(time);
         }
     }
     
     const resetTimer = () => {
         setTimer("00:00:00");
-        stoptime = true;
+        setStoptime(false);
+        // stoptime = false;
         clearTimeout(time);
         hr = 0;
         sec = 0;
